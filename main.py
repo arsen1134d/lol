8c5080b82794d14f013125edb13afe030d7e56fa
arsen <ark@gmail.com>
2023-11-14 21:39:08 +0000

    .

--- main.py
+++ main.py
@@ -1,50 +1,53 @@
-class t:
-   total_t = 0
+# class Shifrator:
+#
+#     def __init__(self,a, b):
+#         self.__a = a
+#         self.__b = b
+#
+#     def __result(self):
+#         return self.__a * self.__b/5
+#
+#     def get_result(self):
+#         return self.__result()
+#
+#
+# sh = Shifrator(5,6 )
+# print(sh.get_result())
 
 
-
-   def __init__(self, name, swim = False, fly=False, walk = True, run = True):
-       self.name = name
-       self.can_swim = swim
-       self.can_fly = fly
-       self.can_walk = walk
-       self.can_run = run
-
-
-def swim(self):
-        if self.can_swim:
-            return f"{self.name}"
-        else:
-            return f"{self.name}"
-
-def fly(self):
-       if self.can_fly:
-           return f"{self.name}"
-       else:
-           return f"{self.name}"
-
- def walk(self):
-       if self.can_walk:
-           return f"{self.name}"
-       else:
-           return f"{self.name}"
-
-
-
-def run(self):
-    if self.can_run:
-        return f"{self.name}"
-    else:
-        return f"{self.name}"
-
-tiger = t(swim = False, fly =False, walk = True, run = True)
-duck = t(swim= True, fly =True, walk = True, run = False)
-fish = t(swim = True, fly =False, walk = False, run = False)
-monkey = t(swim = True, fly =False, walk = True, run =True)
-
-print(tiger.swim())
-print(duck.swim())
-print(fish.swim())
-print(monkey.swim())
-
-print(f"зоопарк: {t.total_t}")
+#
+# class Number1:
+#     test_number = 3
+#
+#     def __init__(self, number, number_2):
+#         self._number1 = number
+#         self._number2 = number_2
+#         self._test_number2 = 1
+#         self._test_number3 = 2
+#
+#     def __privat_numbers(self, number_3):
+#         self._number3 = number_3
+#         self._number4 = 4
+#         self._number34 = self.__number3 + self._number4
+#
+#     def __not_privat(self, number5 ):
+#         self._number5 = number5
+#         self._number6 = 6
+#
+# class Number(Number1):
+#
+#     def __privat_numbers(self, number7):
+#         self.__number7 = number7
+#         self.__number8 = 8
+#         print(self.__number7 - self.__number8 - self._number11)
+#
+#     def __not_privat(self, number9):
+#         self._number9 = number9
+#         self._number10 = 10
+#         print(self._number9 + self._number10 + self._number11)
+#
+#
+# number = Number(3,4)
+# number._not_privat_numbers2(10)
+# number2 = Number1(4, 2)
+# number2._not_privat_number(90)
